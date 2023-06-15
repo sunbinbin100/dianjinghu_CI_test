c723742f5de29e2202dac6c6a841a83f39bad4b6
sun <191415770@qq.com>
2023-06-15 14:34:15 +0800

    陈冬123

--- Test_dianjinghu.py
+++ Test_dianjinghu.py
@@ -1,28 +1,50 @@
 # encoding: utf-8
 # @Author  : sun
 
-import datetime
-import json, logging
-import random, requests
-import re, sys, copy, ast, time
-from prettyprinter import cpprint
-from functools import reduce
-from collections import defaultdict, OrderedDict, namedtuple, deque, Counter
-from collections.abc import Iterable, Iterator
+import requests
+# import datetime, random
+# import json, logging
+# import re, sys, copy, ast, time
+# from prettyprinter import cpprint
+# from functools import reduce
+# from collections import defaultdict, OrderedDict, namedtuple, deque, Counter
+# from collections.abc import Iterable, Iterator
 
 
-# 测试用
-def get_djh_user_info(phone, r_l, typ):
-    payload0 = {"mobile": phone, "password": 'e10adc3949ba59abbe56e057f20f883e', 'remember_login': r_l, 'type': typ}
-    respo1 = requests.post(url='https://www.dianjinghu.com/web.php?m=home&c=login&a=log', data=payload0)
-    # print(respo1.json())
-    params = {'m': 'home', 'c': 'memberNew', 'a': 'center'}
-    respo3 = requests.get(url='https://www.dianjinghu.com/web.php?m=home&c=memberNew&a=center', cookies=respo1.cookies, params=params)
-    return respo3.json()
-    # 更改昵称：{"nickname": "帆布鞋丶"}   web.php/home/member/chNickname/
+# def get_djh_user_info(phone, r_l, typ):
+#     """查看用户信息"""
+#     payload = {"mobile": phone, "password": 'e10adc3949ba59abbe56e057f20f883e', 'remember_login': r_l, 'type': typ}
+#     respo1 = requests.post(url='https://www.dianjinghu.com/web.php?m=home&c=login&a=log', data=payload)
+#     # print(respo1.json())
+#     params = {'m': 'home', 'c': 'memberNew', 'a': 'center'}
+#     respo3 = requests.get(url='https://www.dianjinghu.com/web.php?m=home&c=memberNew&a=center', cookies=respo1.cookies, params=params)
+#     return respo3.json()
+
+def change_djh_nickname(nickname):
+    """更改昵称"""
+    payload0 = {"mobile": 15221466224, "password": 'e10adc3949ba59abbe56e057f20f883e', 'remember_login': 1, 'type': 1}
+    resp1 = requests.post(url='https://www.dianjinghu.com/web.php?m=home&c=login&a=log', data=payload0)
+    # print(resp1.json())
+    payload1 = {"nickname": str(nickname)}
+    resp2 = requests.post(url='https://www.dianjinghu.com/web.php/home/member/chNickname/', cookies=resp1.cookies, data=payload1)
+    return resp2.json()
 
 
-cpprint(get_djh_user_info(15221466224, 1, 1))
+print(change_djh_nickname('陈冬123'))
+
+
+# payload0 = {"mobile": 15221466224, "password": 'e10adc3949ba59abbe56e057f20f883e', 'remember_login': 1, 'type': 1}
+# resp1 = requests.post(url='https://www.dianjinghu.com/web.php?m=home&c=login&a=log', data=payload0)
+# # print(resp1.json())
+# payload1 = {"nickname": '帆布鞋1'}
+# resp2 = requests.post(url='https://www.dianjinghu.com/web.php/home/member/chNickname/', cookies=resp1.cookies, data=payload1)
+# print(resp2.json())
+
+
+
+
+
+
 
 
 
