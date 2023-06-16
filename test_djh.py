8c8c5f698a8484c1f7b6b141fb36fce5bb1ab245
sun <191415770@qq.com>
2023-06-16 16:08:54 +0800

    sun23

--- test_djh.py
+++ test_djh.py
@@ -33,7 +33,7 @@ def change_djh_nickname(mobile, nickname):
     return resp2.json()
 
 
-print(change_djh_nickname(15221466224, 'sun22'))
+print(change_djh_nickname(15221466224, 'sun23'))
 print("运行成功~")
 
 # if __name__ == "__main__":
