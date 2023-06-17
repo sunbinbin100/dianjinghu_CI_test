8fff8fd3e983c7995dba5e2dd5dcffd65d912a3f
sun <191415770@qq.com>
2023-06-18 00:15:08 +0800

    sun26

--- test_djh.py
+++ test_djh.py
@@ -20,21 +20,21 @@ from prettyprinter import cpprint
 #     return respo3.json()
 
 def change_djh_nickname(mobile, nickname):
-    """更改昵称"""
+    """更改昵称接口"""
+    # 先登录，提供cookie
     payload0 = {"mobile": mobile, "password": 'e10adc3949ba59abbe56e057f20f883e', 'remember_login': 1, 'type': 1}
     resp1 = requests.post(url='https://www.dianjinghu.com/web.php?m=home&c=login&a=log', data=payload0)
     # print(resp1.json())
     payload1 = {"nickname": str(nickname)}
     resp2 = requests.post(url='https://www.dianjinghu.com/web.php/home/member/chNickname/', cookies=resp1.cookies, data=payload1)
     # 输出用户信息，查看更改昵称是否成功
-    params = {'m': 'home', 'c': 'memberNew', 'a': 'center'}
-    resp3 = requests.get(url='https://www.dianjinghu.com/web.php?m=home&c=memberNew&a=center', cookies=resp1.cookies, params=params)
+    params1 = {'m': 'home', 'c': 'memberNew', 'a': 'center'}
+    resp3 = requests.get(url='https://www.dianjinghu.com/web.php?m=home&c=memberNew&a=center', cookies=resp1.cookies, params=params1)
     cpprint(resp3.json())
     return resp2.json()
 
 
-print(change_djh_nickname(15221466224, 'sun25'))
-print("运行成功~~~")
+print(change_djh_nickname(15221466224, 'sun26'))
 
 # if __name__ == "__main__":
 #     print(change_djh_nickname(15221466224, 'sun20'))
